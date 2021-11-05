40dd50690937cb3f8adb644363b4830ecd7fcfcc
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2021-11-05 19:23:17 +0100

    Format pad transform

--- text_recognizer/data/transforms/pad.py
+++ text_recognizer/data/transforms/pad.py
@@ -14,7 +14,7 @@ class Pad:
 
     def __call__(self, y: Tensor) -> Tensor:
         """Pads sequences with pad index if shorter than max len."""
-        if y.shape[-1] < self.length:
+        if y.shape[-1] < self.max_len:
             pad_len = self.max_len - len(y)
             y = torch.cat((y, torch.LongTensor([self.pad_index] * pad_len)))
         return y[: self.max_len]
