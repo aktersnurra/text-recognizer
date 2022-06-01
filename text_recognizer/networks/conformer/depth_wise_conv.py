90f7e3882610945077949b96a011c92a8e7c8106
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-06-01 23:15:54 +0200

    Format

--- text_recognizer/networks/conformer/depth_wise_conv.py
+++ text_recognizer/networks/conformer/depth_wise_conv.py
@@ -5,8 +5,13 @@ from torch import nn, Tensor
 class DepthwiseConv1D(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int) -> None:
         super().__init__()
-        self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, groups=in_channels,
-                padding="same")
+        self.conv = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            groups=in_channels,
+            padding="same",
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         return self.conv(x)
--- text_recognizer/networks/conformer/glu.py
+++ text_recognizer/networks/conformer/glu.py
@@ -6,7 +6,7 @@ class GLU(nn.Module):
     def __init__(self, dim: int) -> None:
         super().__init__()
         self.dim = dim
-        
+
     def forward(self, x: Tensor) -> Tensor:
         out, gate = x.chunk(2, dim=self.dim)
         return out * gate.sigmoid()
