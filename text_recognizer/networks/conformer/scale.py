729867b14754bffd98a3406b5d3a2eb9d09bc3bc
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-06-01 23:11:05 +0200

    WIP conformer

---
+++ text_recognizer/networks/conformer/scale.py
@@ -0,0 +1,13 @@
+"""Scale layer."""
+from typing import Dict
+from torch import nn, Tensor
+
+
+class Scale(nn.Module):
+    def __init__(self, scale: float, fn: nn.Module) -> None:
+        super().__init__()
+        self.scale = scale
+        self.fn = fn
+
+    def forward(self, x: Tensor, kwargs: Dict) -> Tensor:
+        return self.fn(x, **kwargs) * self.scale
