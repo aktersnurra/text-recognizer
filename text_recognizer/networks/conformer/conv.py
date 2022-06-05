425af1bce8362efd97682a5042e76a60bfc28060
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-06-05 21:18:56 +0200

    Remove depth wise conv class

--- text_recognizer/networks/conformer/conv.py
+++ text_recognizer/networks/conformer/conv.py
@@ -4,7 +4,6 @@ from einops.layers.torch import Rearrange
 from torch import nn, Tensor
 
 
-from text_recognizer.networks.conformer.depth_wise_conv import DepthwiseConv1D
 from text_recognizer.networks.conformer.glu import GLU
 
 
@@ -21,12 +20,18 @@ class ConformerConv(nn.Module):
         self.layers = nn.Sequential(
             nn.LayerNorm(dim),
             Rearrange("b n c -> b c n"),
-            nn.Conv1D(dim, 2 * inner_dim, 1),
+            nn.Conv1d(dim, 2 * inner_dim, 1),
             GLU(dim=1),
-            DepthwiseConv1D(inner_dim, inner_dim, kernel_size),
+            nn.Conv1d(
+                in_channels=inner_dim,
+                out_channels=inner_dim,
+                kernel_size=kernel_size,
+                groups=inner_dim,
+                padding="same",
+            ),
             nn.BatchNorm1d(inner_dim),
             nn.Mish(inplace=True),
-            nn.Conv1D(inner_dim, dim, 1),
+            nn.Conv1d(inner_dim, dim, 1),
             Rearrange("b c n -> b n c"),
             nn.Dropout(dropout),
         )
