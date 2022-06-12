8bb76745e43c6b4967c8e91ebaf4c4295d0b8d0b
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-06-12 23:16:20 +0200

    Remove conformer

--- text_recognizer/networks/conformer/__init__.py
+++
@@ -1,7 +0,0 @@
-from text_recognizer.networks.conformer.block import ConformerBlock
-from text_recognizer.networks.conformer.ff import Feedforward
-from text_recognizer.networks.conformer.glu import GLU
-from text_recognizer.networks.conformer.conformer import Conformer
-from text_recognizer.networks.conformer.conv import ConformerConv
-from text_recognizer.networks.conformer.subsampler import Subsampler
-from text_recognizer.networks.conformer.attention import Attention
