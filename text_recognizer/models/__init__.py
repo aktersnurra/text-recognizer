8b2e5296b290f147935c58207fbfd9674394c7b3
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-09-13 18:45:36 +0200

    Remove vq and perceiver models

--- text_recognizer/models/__init__.py
+++ text_recognizer/models/__init__.py
@@ -1,4 +1,2 @@
 """PyTorch Lightning models modules."""
 from text_recognizer.models.transformer import LitTransformer
-from text_recognizer.models.perceiver import LitPerceiver
-from text_recognizer.models.vq_transformer import LitVqTransformer
