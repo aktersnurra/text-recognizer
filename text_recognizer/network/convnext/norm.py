1732ed564a738a42c1bf6e8127ae810f5658cb06
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2023-09-03 22:54:09 +0200

    Revert "Delete convnext"
    This reverts commit 7239bce214607c70a7a91358586f265b2f74de7b.

---
+++ text_recognizer/network/convnext/norm.py
@@ -0,0 +1,18 @@
+"""Layer norm for conv layers."""
+import torch
+from torch import Tensor, nn
+
+
+class LayerNorm(nn.Module):
+    """Layer norm for convolutions."""
+
+    def __init__(self, dim: int) -> None:
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(1, dim, 1, 1))
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Applies layer norm."""
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+        var = torch.var(x, dim=1, unbiased=False, keepdim=True)
+        mean = torch.mean(x, dim=1, keepdim=True)
+        return (x - mean) / (var + eps).sqrt() * self.gamma
